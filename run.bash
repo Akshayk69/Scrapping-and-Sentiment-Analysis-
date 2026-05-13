eb8fb15e10eacb76e77a8444614c97fa233f51ae
Akshay Kumar <94836744+Akshayk69@users.noreply.github.com>
2026-05-13 20:37:18 +0530

    Delete run.bash

--- run.bash
+++
@@ -1,7 +0,0 @@
-docker run --rm --init \
-   --name dbhub \
-   --publish 8080:8080 \
-   bytebase/dbhub \
-   --transport http \
-   --port 8080 \
-   --dsn "postgres://user:password@localhost:5432/dbname?sslmode=disable"
